e203533406c8731256c118d39df81ad54f71ec89
ttaarroo77 <nakazawatarou@gmail.com>
2025-02-27 11:49:22 +0900

    Simplify Solid Queue configuration to minimum

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,6 +1,3 @@
 Rails.application.config.solid_queue.connects_to = {
-  database: { writing: :primary, reading: :primary }
+  database: { writing: :primary }
 }
-
-Rails.application.config.solid_queue.concurrency = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-Rails.application.config.solid_queue.dispatch_interval = 1
